51448e8d4f97701294bca787838b3902bfe852b4
Chambers <964089@swansea.ac.uk>
2020-10-18 02:15:09 +0100

    1) Program flow 1, done up to L

---
+++ Programming Tasks Sheet 1/Assets/myname.cs
@@ -0,0 +1,42 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myname : MonoBehaviour
+{
+    private Camera gameCamera; // Is this the simple camera controller script being refered to ?  
+    public string myName = "964089"; //  ??is this what is meant by hard-coding? Can you hard-code in Unity?
+    public string Name = "Elsa";
+    private int A = 1, B = 2;
+   
+        
+
+
+    // Awake is a built-in Unity function that is only called once, before the Start function
+    private void Awake()
+    {
+        gameCamera = FindObjectOfType<Camera>();
+       
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("and my student number is");
+
+        Debug.Log("myName"); // one-time Debug.Log conttaning what i hope was hardcoding in line 9 
+
+        Debug.Log("Chambers"); // because  the Start is called before any of the Update methods are called 
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
