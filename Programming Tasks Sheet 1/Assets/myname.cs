8e2cd3de26fca6a1a7606453e45d8e8eb293798b
Chambers <964089@swansea.ac.uk>
2020-10-18 12:49:51 +0100

    Updated changes made to Program flow. Parts b. to k. have been done in this commit.

--- Programming Tasks Sheet 1/Assets/myname.cs
+++ Programming Tasks Sheet 1/Assets/myname.cs
@@ -7,10 +7,10 @@ using UnityEngine;
 public class myname : MonoBehaviour
 {
     private Camera gameCamera; // Is this the simple camera controller script being refered to ?  
-    public string myName = "964089"; //  ??is this what is meant by hard-coding? Can you hard-code in Unity?
+    public string StudentNumber = "964089"; // Is this what is meant by hard-coding? Can you hard-code in Unity?
     public string Name = "Elsa";
     private int A = 1, B = 2;
-   
+
         
 
 
@@ -24,12 +24,11 @@ public class myname : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("Chambers"); // This is because  the Start is called before any of the Update methods are called 
+       
         Debug.Log("and my student number is");
 
-        Debug.Log("myName"); // one-time Debug.Log conttaning what i hope was hardcoding in line 9 
-
-        Debug.Log("Chambers"); // because  the Start is called before any of the Update methods are called 
-        
+        Debug.Log("StudentNumber"); // one-time Debug.Log containing what i hope was hardcoding in line 9
 
     }
 
