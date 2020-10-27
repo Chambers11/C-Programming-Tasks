6591bc0fc191cc5c7cdbb097d812fb46e4b5ec55
Chambers <964089@swansea.ac.uk>
2020-10-27 13:43:57 +0000

    Generated a floting point number for (10/3).

--- Programming Tasks Sheet 1/Assets/HelloTextMeshPro.cs
+++ Programming Tasks Sheet 1/Assets/HelloTextMeshPro.cs
@@ -8,18 +8,20 @@ public class HelloTextMeshPro : MonoBehaviour
     public TMPro.TMP_Text text_display;
     public string Name;
 
-    // Start is called before the first frame update
+    // Start is called before the first frame update.
     void Start()
     {
-        // The Name display in editor doesn't allow you to change the Name once the program has been started becase the Start is called just before any of the Update methods  
+        // The Name display in editor doesn't allow you to change the Name once the program has been started becase the Start is called just before any of the Update methods.  
         text_display.text = Name;
+
+        Name += (10 / 3f); // The use of f allows the value to be recognised as a floating point number, without the f the default is to take the value as an integer.
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // The Name display in editor  now allows you to change the Name once the program has been started, this is because Update is called every frame, if Mono behaviour is enabled   
+        // The Name display in editor  now allows you to change the Name once the program has been started, this is because Update is called every frame, if Mono behaviour is enabled.   
         text_display.text = Name;
-
     }
 }
