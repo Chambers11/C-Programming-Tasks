02eba45bc6652c542c3b0bf1f235e78c3163e514
Chambers <964089@swansea.ac.uk>
2020-10-28 08:09:47 +0000

    Started 6, have managed to finish the FizzBuzz section, (Thank you Tom Scott). Am going to write another version of code for the FizzBuzz game for interview purposes.

---
+++ Programming Tasks Sheet 1/Assets/LoopsAndIfs.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopsAndIfs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (var i = 1; i <= 100; i++)
+        {
+            if ((i%3== 0) && (i%5==0))
+            {
+                Debug.Log("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Debug.Log("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Debug.Log("Buzz");
+            }
+            else
+            {
+                Debug.Log(i);
+            }
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    } 
+}
