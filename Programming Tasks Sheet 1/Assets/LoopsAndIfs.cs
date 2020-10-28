6fcf46404848e8088dc45fbeee91833953cae487
Chambers <964089@swansea.ac.uk>
2020-10-28 08:48:18 +0000

    Better version of FizzBuzz, updated and running well.

--- Programming Tasks Sheet 1/Assets/LoopsAndIfs.cs
+++ Programming Tasks Sheet 1/Assets/LoopsAndIfs.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class LoopsAndIfs : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    // This version of code according to Tom Scott is bad practice if you choose to make the section of code larger becuase if you want the game to work for multiples of 7 you'd have to manual change all the numbers e.g. 5 to 7!!
+  private  void Start()
     {
-        for (var i = 1; i <= 100; i++)
+      /*  for (var i = 1; i <= 100; i++)
         {
             if ((i%3== 0) && (i%5==0))
             {
@@ -24,9 +25,28 @@ public class LoopsAndIfs : MonoBehaviour
             else
             {
                 Debug.Log(i);
+            }*/
+      // Based on what Tom Scott said this is a better way of writing code for the FizzBuzz game, as it allows you to easiy change the multiples from 5 to 7 to any number really.It also allows you to add more numbers
+      for (var i = 1; i <= 100; i++)
+        {
+            var output = ""; 
+            if (i % 3 == 0)
+            {
+                output += "Fizz";
             }
-
-        }
+            if (i % 5 == 0)
+            {
+                output += "Buzz";
+            }
+           if (output == "") 
+            {
+                // Had to use .ToString() to allow output string to accept int i (Not sure why it worked but it did)
+                output = (i).ToString();
+                
+            }
+                Debug.Log(output);
+        } 
+    }
 
         // Update is called once per frame
         void Update()
@@ -34,4 +54,4 @@ public class LoopsAndIfs : MonoBehaviour
 
         }
     } 
-}
+
